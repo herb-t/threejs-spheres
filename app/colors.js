43dd42a8f550a9359e543aac2be70dcdcc714937
herb-t <herb.torres@byhook.com>
2015-07-23 12:42:44 -0400

    initial commit

---
+++ app/colors.js
@@ -0,0 +1,9 @@
+var colors = {
+    'red': 'font-weight: bold; color: #EC5f67;',
+    'orange': 'font-weight: bold; color: #F99157;',
+    'yellow': 'font-weight: bold; color: #FAC863;',
+    'green': 'font-weight: bold; color: #99C794;',
+    'purple': 'font-weight: bold; color: #C594C5;'
+};
+
+module.exports = colors;
